f0557354e487135dbfc74d814fc9cf9a8ccda73d
Jean-Baptiste <ultrapowa.exp@gmail.com>
2022-05-05 18:45:56 +0200

    expose event emitter

--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -2,10 +2,12 @@ const Broker = require('./broker');
 const Worker = require('./worker');
 const TcpServer = require('./tcp-server');
 const TcpGateway = require('./tcp-gateway');
+const SafeEventEmitter = require('./safe-event-emitter');
 
 module.exports = {
   Broker,
   Worker,
   TcpServer,
   TcpGateway,
+  SafeEventEmitter,
 };
